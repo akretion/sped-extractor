285a05a4f22b5f81ab9855de2484f372b0be4830
clementmbr <clement.mombereau@akretion.com.br>
2020-07-02 22:22:33 -0300

    [ADD] setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+
+from setuptools import setup
+from spedextractor import __version__
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.rst").read_text()
+
+setup(
+    name="sped-extractor",
+    version=__version__,
+    description=(
+        "Extrai e interpreta a integralidade dos registros e campos das tabelas dos "
+        "manuais do SPED (Sistema Público de Escrituração Digital), para os módulos "
+        "ECD, ECF, EFD Contribuições (PIS, COFINS) e EFD ICMS IPI."
+    ),
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/akretion/sped-extractor",
+    author="Akretion",
+    author_email="contact@akretion.com.br",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["spedextractor"],
+    include_package_data=True,
+    install_requires=[r.strip() for r in open("requirements.txt").read().splitlines()],
+)
